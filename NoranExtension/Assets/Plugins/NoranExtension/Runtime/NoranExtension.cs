ec3fc320c366198ff6852ec5702123c2dff459d7
Masorm <fw_yama9@yahoo.co.jp>
2022-09-04 01:21:34 +0900

    add: Base NoranExtension struct

---
+++ NoranExtension/Assets/Plugins/NoranExtension/Runtime/NoranExtension.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+
+namespace Noran.Extension
+{
+    /// <summary>
+    /// UNITYの基本的な機能をまとめる
+    /// </summary>
+    public partial struct NoranExtension
+    {
+        /// <summary>
+        /// Editor上でのみオブジェクトの名前をDebug.Logに吐き出す
+        /// </summary>
+        /// <param name="target"></param>
+        /// <typeparam name="T"></typeparam>
+        [Conditional("UNITY_EDITOR")]
+        public static void DebugLogObjectName<T>(T target) where T : Object
+        {
+            Debug.Log($"[ObjectName]: {target.name}");
+        }
+
+        /// <summary>
+        /// Editor上でのみメソッドの名前をDebug.Logに吐き出す
+        /// </summary>
+        /// <param name="action"></param>
+        [Conditional("UNITY_EDITOR")]
+        public static void DebugLogActionName(Action action)
+        {
+            Debug.Log($"[ActionName]: {action.Method.Name}");
+            action();
+        }
+    }
+}
